241518467eccd00bc37d4c730fb0b95a520e307e
Spl3en <spl3en.contact@gmail.com>
2017-11-01 23:03:45 +0100

    Patch 177106_001001 [Files added : 7221] - [Files modified : 0] - [Lines added : 1092361] - [Lines removed : 0]

--- shared.ipf/script/DialogPreCheck/f_gele_57_1.lua
+++ shared.ipf/script/DialogPreCheck/f_gele_57_1.lua
@@ -5,4 +5,12 @@ function SCR_GELE571_RP_1_OBJ_PRE_DIALOG(pc, dialog)
     end
     return 'NO'
 end
-
+
+function SCR_GELE571_MQ_03_PRE_DIALOG(pc, dialog)
+    local result = SCR_QUEST_CHECK(pc, 'GELE571_MQ_03')
+    if result == 'PROGRESS' then
+        return 'YES'
+    end
+    return 'NO'
+end
+
