099d6b0e944859994abfdd213b57ca4de6417024
Spl3en <spl3en.contact@gmail.com>
2018-03-27 04:26:58 +0200

    Patch 195300_001001

--- script_client.ipf/pc_client.lua
+++ script_client.ipf/pc_client.lua
@@ -1,7 +1,28 @@
--- pc_clinet.lua
+Ôªø-- pc_clinet.lua
 
 function GET_CASH_TOTAL_POINT_C()
 	local aobj = GetMyAccountObj();
-	-- PremiumMedal : ¿Ø¿˙ ±∏∏≈ TP, Medal : π´∑·TP, GiftMedal : ≥ÿΩº service TP
+	-- PremiumMedal : Ïú†Ï†Ä Íµ¨Îß§ TP, Medal : Î¨¥Î£åTP, GiftMedal : ÎÑ•Ïä® service TP
 	return aobj.Medal + aobj.GiftMedal + aobj.PremiumMedal;
+end
+-- pcÏùò partymember Ïò§Î∏åÏ†ùÌä∏Î•º Í∞ÄÏ†∏Ïò§Îäî Ìï®Ïàò
+function GET_MY_PARTY_INFO_C()
+	local myInfo = session.party.GetMyPartyObj(PARTY_NORMAL);
+	if myInfo == nil then
+		return;
+	end	
+	return GetIES(myInfo:GetObject());	
+end
+
+function LEVEL_LINFORM_MESSAGE_CLIENT(idList)
+--    print('FFFFFFFFFFFFFFF',idList)
+    local idList = SCR_STRING_CUT(idList)
+    local msg = '{img lvup_guide_banner}'
+    for i = 1, #idList do
+        local ies = GetClassByType('levelinformmessage', idList[i])
+        msg = msg..'{nl} {nl}'..i..'. '..ies.Message
+    end
+--    ui.MsgBox_NonNested(msg,0x00000000)
+    ui.MsgBox_NonNested(msg,0x00000000, nil, 'None', 'None', -300)
+--    print('HHHHHHHHHHHHHHH')
 end
