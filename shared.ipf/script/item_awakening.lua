81466b25fadff95bedb263350579367a845a227e
Spl3en <spl3en.contact@gmail.com>
2018-06-07 10:31:33 +0200

    Patch 205228_001001

---
+++ shared.ipf/script/item_awakening.lua
@@ -0,0 +1,21 @@
+-- item_awakening.lua
+
+
+function GET_ITEM_AWAKENING_PRICE(obj) -- hs_comment: 안용씨 여기 각성 가루 아이템의 클래스 네임과 개수 공식을 써주십쇼    
+	local count = 1;
+--    if obj ~= nil then
+--        count = obj.ItemStar * obj.ItemGrade;
+--    end
+    
+	return "misc_wakepowder", count;
+end
+
+function IS_ITEM_AWAKENING_STONE(obj)
+	if obj.ClassName == "misc_awakeningStone1" then
+		return true;
+	elseif obj.ClassName == "Premium_awakeningStone" or obj.ClassName == "Premium_awakeningStone14" or obj.ClassName == "Premium_awakeningStone_TA" then
+		return true;
+	end
+
+	return false;
+end
