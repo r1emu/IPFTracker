9fb94a6c729be05c6e67816fbdd3697c4a25a5d5
Spl3en <spl3en.contact@gmail.com>
2018-09-04 05:30:43 +0200

    Patch 217403_001001

--- shared.ipf/script/DialogPreCheck/custom_precheck.lua
+++ shared.ipf/script/DialogPreCheck/custom_precheck.lua
@@ -18,6 +18,7 @@ function SCR_JOB_2_HUNTER_4_1_BOX_1_PRE_DIALOG(pc, dialog)
     if result1 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 function SCR_JOB_2_HUNTER_4_1_BOX_2_PRE_DIALOG(pc, dialog)
@@ -25,6 +26,7 @@ function SCR_JOB_2_HUNTER_4_1_BOX_2_PRE_DIALOG(pc, dialog)
     if result1 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 function SCR_JOB_2_HUNTER_4_1_BOX_3_PRE_DIALOG(pc, dialog)
@@ -32,41 +34,80 @@ function SCR_JOB_2_HUNTER_4_1_BOX_3_PRE_DIALOG(pc, dialog)
     if result1 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 
 
 function SCR_JOB_2_PSYCHOKINO_5_1_BOOK_1_PRE_DIALOG(pc, dialog)
-    local result1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
-    if result1 == 'PROGRESS' then
+    local questCheck1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
+    local questCheck2 = SCR_QUEST_CHECK(pc, 'MASTER_PSYCHOKINO1')
+    if questCheck1 == 'PROGRESS' or questCheck2 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 function SCR_JOB_2_PSYCHOKINO_5_1_BOOK_2_PRE_DIALOG(pc, dialog)
-    local result1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
-    if result1 == 'PROGRESS' then
+    local questCheck1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
+    local questCheck2 = SCR_QUEST_CHECK(pc, 'MASTER_PSYCHOKINO1')
+    if questCheck1 == 'PROGRESS' or questCheck2 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 function SCR_JOB_2_PSYCHOKINO_5_1_BOOK_3_PRE_DIALOG(pc, dialog)
-    local result1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
-    if result1 == 'PROGRESS' then
+    local questCheck1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
+    local questCheck2 = SCR_QUEST_CHECK(pc, 'MASTER_PSYCHOKINO1')
+    if questCheck1 == 'PROGRESS' or questCheck2 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 function SCR_JOB_2_PSYCHOKINO_5_1_BOOK_4_PRE_DIALOG(pc, dialog)
-    local result1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
-    if result1 == 'PROGRESS' then
+    local questCheck1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
+    local questCheck2 = SCR_QUEST_CHECK(pc, 'MASTER_PSYCHOKINO1')
+    if questCheck1 == 'PROGRESS' or questCheck2 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
 end
 
 function SCR_JOB_2_PSYCHOKINO_5_1_BOOK_5_PRE_DIALOG(pc, dialog)
-    local result1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
-    if result1 == 'PROGRESS' then
+    local questCheck1 = SCR_QUEST_CHECK(pc, 'JOB_2_PSYCHOKINO_5_1')
+    local questCheck2 = SCR_QUEST_CHECK(pc, 'MASTER_PSYCHOKINO1')
+    if questCheck1 == 'PROGRESS' or questCheck2 == 'PROGRESS' then
         return 'YES'
     end
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_CMINE_66_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_UNDER_59_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_CASTLE_67_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_UNDER_68_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_STOWER_60_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_VPRISON_54_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
+end
+
+function SCR_JOB_MIKO_6_1_FTOWER_61_1_PRE_DIALOG(pc, dialog)
+    return 'NO'
 end
