c9c8d8ab4cc73ac8869ee1108027aa84df10b9d3
Spl3en <spl3en.contact@gmail.com>
2018-12-28 10:01:05 +0100

    Patch 234350_001001

---
+++ addon.ipf/directormode/directormode.lua
@@ -0,0 +1,47 @@
+
+function DIRECTORMODE_ON_INIT(addon, frame)
+
+	addon:RegisterMsg('DIRECTOR_CAPTION', 'SHOW_DIRECTOR_CAPTION');
+	
+end
+
+function DIRECTORMODE_SIZE_UPDATE(frame)
+	frame:MoveFrame(0, 0);
+	local width = option.GetClientWidth()
+    if width < 1920 then width = 1920 end    
+    local ratio = option.GetClientHeight() / option.GetClientWidth()
+	local height = width * ratio
+	frame:Resize(width, height)  
+	local screenmask = frame:GetChild('screenmask')
+	if screenmask ~= nil then
+		screenmask:Resize(width, height)  
+	end
+	frame:Invalidate();
+end
+
+
+function DIRECTORMODE_OPEN(frame)
+
+  DIRECTORMODE_SIZE_UPDATE(frame)
+	frame:Invalidate();
+	local cap = frame:GetChild("caption");
+	if cap == nil then
+		return;
+	end
+	
+	cap:ShowWindow(0);
+	
+end
+
+function SHOW_DIRECTOR_CAPTION(frame, msg, argStr, argNum)
+
+	local cap = frame:GetChild("caption");
+	if cap == nil then
+		return;
+	end
+	
+	cap:SetText("{s20}" .. argStr );
+	cap:ShowWindow(1);
+	frame:Invalidate();
+
+end
