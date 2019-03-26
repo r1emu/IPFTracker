86d8a3a9f2d70da6a8569251ce60cede25fd741f
Spl3en <spl3en.contact@gmail.com>
2019-03-26 06:51:46 +0100

    Patch 243103_001001

--- global.ipf/script/sessionobject/ssn_klapeda.lua
+++ global.ipf/script/sessionobject/ssn_klapeda.lua
@@ -1,42 +1,37 @@
-﻿function SCR_SSN_KLAPEDA_KillMonster_PARTY(self, party_pc, sObj, msg, argObj, argStr, argNum)
-    if SHARE_QUEST_PROP(self, party_pc) == true then
-            if GetLayer(self) == GetLayer(party_pc) then
-                SCR_SSN_KLAPEDA_KillMonster_Sub(self, sObj, msg, argObj, argStr, argNum)
-            end
-    end
+﻿-- function SCR_SSN_KLAPEDA_KillMonster_PARTY(self, party_pc, sObj, msg, argObj, argStr, argNum)
+--     if SHARE_QUEST_PROP(self, party_pc) == true then
+--             if GetLayer(self) == GetLayer(party_pc) then
+--                 SCR_SSN_KLAPEDA_KillMonster_Sub(self, sObj, msg, argObj, argStr, argNum)
+--             end
+--     end
 
-end
+-- end
 
-function SCR_SSN_KLAPEDA_KillMonster(self, sObj, msg, argObj, argStr, argNum)
-	PC_WIKI_KILLMON(self, argObj, true);
-	CHECK_SUPER_DROP(self);
-  CHECK_CHALLENGE_MODE(self, argObj);
+-- function SCR_SSN_KLAPEDA_KillMonster(self, sObj, msg, argObj, argStr, argNum)
+-- 	PC_WIKI_KILLMON(self, argObj, true);
+-- 	CHECK_SUPER_DROP(self);
+--   CHECK_CHALLENGE_MODE(self, argObj);
   
-	SCR_SSN_KLAPEDA_KillMonster_Sub(self, sObj, msg, argObj, argStr, argNum)
+-- 	SCR_SSN_KLAPEDA_KillMonster_Sub(self, sObj, msg, argObj, argStr, argNum)
 
-	if IsIndun(self) == 1 then
-		IndunMonKillCountIncrease(self);
-	end
-	-- EVENT_1811_NEWCHARACTER
-    SCR_SSN_EVENT_1811_NEWCHARACTER_KillMonster(self, sObj, msg, argObj, argStr, argNum)
+-- 	if IsIndun(self) == 1 then
+-- 		IndunMonKillCountIncrease(self);
+-- 	end
     
-    -- EVENT_1811_KUPOLE
-    SCR_SSN_EVENT_1811_KUPOLE_KillMonster(self, sObj, msg, argObj, argStr, argNum)
-    
----- ID_WHITETREES1
-    if GetZoneName(self) == 'id_whitetrees1' then
-        if argObj.ClassName == 'ID_umblet' then
-            if IMCRandom(1, 10000) < 601 then
-                RunScript('GIVE_ITEM_TX', self, 'misc_id_330_gimmick_01', 1, 'INDUN_330')
-            end
-        elseif argObj.ClassName == 'ID_kucarry_Tot' then
-            if IMCRandom(1, 10000) < 601 then
-                RunScript('GIVE_ITEM_TX', self, 'misc_id_330_gimmick_02', 1, 'INDUN_330')
-            end
-        elseif argObj.ClassName == 'ID_kucarry_lioni' then
-            if IMCRandom(1, 10000) < 601 then
-                RunScript('GIVE_ITEM_TX', self, 'misc_id_330_gimmick_03', 1, 'INDUN_330')
-            end
-        end
-    end
-end
+-- ---- ID_WHITETREES1
+--     if GetZoneName(self) == 'id_whitetrees1' then
+--         if argObj.ClassName == 'ID_umblet' then
+--             if IMCRandom(1, 10000) < 601 then
+--                 RunScript('GIVE_ITEM_TX', self, 'misc_id_330_gimmick_01', 1, 'INDUN_330')
+--             end
+--         elseif argObj.ClassName == 'ID_kucarry_Tot' then
+--             if IMCRandom(1, 10000) < 601 then
+--                 RunScript('GIVE_ITEM_TX', self, 'misc_id_330_gimmick_02', 1, 'INDUN_330')
+--             end
+--         elseif argObj.ClassName == 'ID_kucarry_lioni' then
+--             if IMCRandom(1, 10000) < 601 then
+--                 RunScript('GIVE_ITEM_TX', self, 'misc_id_330_gimmick_03', 1, 'INDUN_330')
+--             end
+--         end
+--     end
+-- end
