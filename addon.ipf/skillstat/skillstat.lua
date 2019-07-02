55062d29fb7e36009b265277ae978878caebbaec
Spl3en <spl3en.contact@gmail.com>
2019-07-02 10:25:59 +0200

    Patch 253362_001001

--- addon.ipf/skillstat/skillstat.lua
+++ addon.ipf/skillstat/skillstat.lua
@@ -65,7 +65,7 @@ function BEFORE_APPLIED_SKILLSTAT_OPEN(invItem)
 end
 
 local function IS_SKILLRESET_ITEM(itemClsName)
-	if itemClsName == 'Premium_SkillReset' or itemClsName == 'Premium_SkillReset_14d' or itemClsName == 'Premium_SkillReset_1d' or itemClsName == 'Premium_SkillReset_60d' or itemClsName == 'Premium_SkillReset_Trade' or itemClsName == 'PC_Premium_SkillReset' or itemClsName == 'Premium_SkillReset_14d_Team' then
+	if itemClsName == 'Premium_SkillReset' or itemClsName == 'Premium_SkillReset_14d' or itemClsName == 'Premium_SkillReset_1d' or itemClsName == 'Premium_SkillReset_60d' or itemClsName == 'Premium_SkillReset_Trade' or itemClsName == 'PC_Premium_SkillReset' or itemClsName == 'Premium_SkillReset_14d_Team' or itemClsName == 'steam_Premium_SkillReset_1' or itemClsName == 'steam_Premium_SkillReset' then
 		return true;
 	end
 	return false;
